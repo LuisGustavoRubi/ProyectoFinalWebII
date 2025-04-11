c9ce9729bcf97a6fc48566115df45e86fc5abd5d
LuisGustavoRubi <luisgustavorubifunez612@gmail.com>
2025-04-11 14:55:03 -0600

    Correccion

--- data/galeriaJSON.js
+++ data/galeriaJSON.js
@@ -3,37 +3,37 @@ export const galeriaJSON = () => {
       {
         id: 1,
         title: "Torneos Majic",
-        image: "/galeryImg/magic1.png", 
+        image: "galeryImg/magic1.png", 
         description: "Torneo de Cartas Majic",
       },
       {
         id: 2,
         title: "Torneos Majic",
-        image: "/galeryImg/magic2.jpg", 
+        image: "galeryImg/magic2.jpg", 
         description: "Finales de Torneo",
       },
       {
         id: 3,
         title: "Torneos Pokemon",
-        image: "/galeryImg/pokemon1.jpg", 
+        image: "galeryImg/pokemon1.jpg", 
         description: "Clasificatoria",
       },
       {
         id: 4,
         title: "Local",
-        image: "/galeryImg/spot2.jpg", 
+        image: "galeryImg/spot2.jpg", 
         description: "Deposito en fisico",
       },
       {
         id: 5,
         title: "Mangas",
-        image: "/galeryImg/spot1.jpg", 
+        image: "galeryImg/spot1.jpg", 
         description: "Gran apertura de sección",
       },
       {
         id: 6,
         title: "Smash",
-        image: "/galeryImg/smash2.jpg", 
+        image: "galeryImg/smash2.jpg", 
         description: "Torneo de Smash",
       },
     ];
